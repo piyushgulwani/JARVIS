d329d421f3ac599309bb8a2ae14cfaba4c1c1552
Piyush Gulwani <dheergulwani1@gmail.com>
2020-12-16 20:04:05 +0530

    testing

--- test.py
+++ test.py
@@ -1,8 +1,16 @@
 import wolframalpha 
 from main import speak
-query = 'Todays Whether'
-app_id = 'API'
-client = wolframalpha.Client(app_id)
-res = client.query(query)
-res1 = (next(res.results).text)[:-1]
-speak(res1)
+# query = 'Todays Whether'
+# app_id = 'API'
+# client = wolframalpha.Client(app_id)
+# res = client.query(query)
+# res1 = (next(res.results).text)[:-1]
+# speak(res1)
+def client_query(quest) : 
+    client = wolframalpha.Client('45EXYP-Q56VVHXWUE')
+    result = client.query(quest)
+    result = (next(result.results).text)
+    result = result[0:6]
+    speak(result)
+
+client_query('1 dollar to ruppee')
