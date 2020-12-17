68542f12a2556a57a6ac769a5e3078f5983ead3c
Piyush Gulwani <dheergulwani1@gmail.com>
2020-12-17 13:40:37 +0530

    Cleaned

--- test.py
+++ test.py
@@ -1,11 +1,13 @@
 import wolframalpha 
 from main import speak
+
 # query = 'Todays Whether'
 # app_id = 'API'
 # client = wolframalpha.Client(app_id)
 # res = client.query(query)
 # res1 = (next(res.results).text)[:-1]
 # speak(res1)
+
 def client_query(quest) : 
     client = wolframalpha.Client('')
     result = client.query(quest)
